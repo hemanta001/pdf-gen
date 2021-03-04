a1597ab5fec76866da8511c024b63a817abefb76
Heman Ghimire <rockingguyheman.hg@gmail.com>
2021-03-04 15:50:35 +0545

    removing unused code

--- src/app/organization/service/organization.service.ts
+++ src/app/organization/service/organization.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class OrganizationService {
   constructor(private http: HttpClient) { }
 
   getOrganizations(){
-    return this.http.get<{ id: number, name: string, selected: boolean }[]>("http://localhost:8080/getOrganizations");
+    return this.http.get<{ id: number, name: string, selected: boolean }[]>(`${environment.baseUrl}getOrganizations`);
   }
 }
